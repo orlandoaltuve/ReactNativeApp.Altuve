07be25e98c75ea702148a2b6964493362ab7c4c4
Orlando Altuve <orlandoaltuve20@gmail.com>
2022-11-06 15:08:16 -0300

    core componets, agregue TextInput, Button y estilos

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hola, Coder!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.itemContainer}>
+      <TextInput  placeholder='Agregar Item a la lista' style={styles.inputItem}/>
+      <Button title='Agregar'/>
+      </View>
+
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding:30,
+    marginTop: 50,
   },
+  itemContainer:{
+    flexDirection:"row",
+    justifyContent:'space-around',
+    alignItems:"center",
+  },
+  inputItem:{
+    borderBottomColor: 'black',
+    borderBottomWidth: 1,
+    width: 200,
+    
+    
+
+
+  }
 });
